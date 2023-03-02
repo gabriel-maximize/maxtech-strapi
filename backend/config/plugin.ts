3b9a92532ba20e0307f6894d3617c07307d81cef
Gabriel <gabriel@pop-os.localdomain>
2023-03-02 14:23:07 -0300

    fix: config geral

---
+++ backend/config/plugin.ts
@@ -0,0 +1,19 @@
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.example.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+      },
+      settings: {
+        defaultFrom: "manutencao@homolog.mx",
+        defaultReplyTo: "gabriel.melo@maximize.art.br",
+      },
+    },
+  },
+});
---
+++ backend/src/api/depoimento/content-types/depoimento/lifecycles.ts
@@ -0,0 +1,17 @@
+export default {
+  async afterCreate(event) {
+    const { result } = event;
+
+    try {
+      await strapi.plugins["email"].services.email.send({
+        to: "manutencao@homolog.mx",
+        from: "manutencao@homolog.mx",
+        subject: "Hello world",
+        text: "Hello world",
+        html: `<h4>Hello world</h4>`,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
