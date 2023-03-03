9a4f8f4c7f92123a088d96fa20bd4eb92d84b347
Gabriel Melo <gabriel.melo@maximize.art.br>
2023-03-03 15:12:21 -0300

    fix: comentado evento de envio de e-mail

--- backend/src/api/depoimento/content-types/depoimento/lifecycles.ts
+++ backend/src/api/depoimento/content-types/depoimento/lifecycles.ts
@@ -1,17 +1,18 @@
 export default {
   async afterCreate(event) {
-    const { result } = event;
+    console.log(event);
+    // const { result } = event;
 
-    try {
-      await strapi.plugins["email"].services.email.send({
-        to: "gabriel.melo@maximize.art.br",
-        from: "manutencao@homolog.mx",
-        subject: "Hello world",
-        text: "Hello world",
-        html: `<h4>Hello world</h4>`,
-      });
-    } catch (error) {
-      console.error(error);
-    }
+    // try {
+    //   await strapi.plugins["email"].services.email.send({
+    //     to: "gabriel.melo@maximize.art.br",
+    //     from: "manutencao@homolog.mx",
+    //     subject: "Hello world",
+    //     text: "Hello world",
+    //     html: `<h4>Hello world</h4>`,
+    //   });
+    // } catch (error) {
+    //   console.error(error);
+    // }
   },
 };
