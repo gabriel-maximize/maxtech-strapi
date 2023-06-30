2ed78c4752f9adb86a3331aaa7f8c8e4020c329c
Gabriel Melo <melogoncalvesbiel@gmail.com>
2023-06-30 09:23:37 -0300

    fix: cache do projeto

--- backend/src/admin/app.tsx
+++ backend/src/admin/app.tsx
@@ -1,17 +1,22 @@
 export default {
   config: {
     theme: {
-      colors: {
-        primary100: '#FF0000',
-        primary200: '#FF0000',
-        primary500: '#FF0000',
-        primary600: '#FF0000',
-        primary700: '#FF0000',
+      light: {
+        colors: {
+          // primary100: "#FF0"
+        }
+      },
+      dark: {
+        colors: {
+          // primary100: "#E5E5E5"
+        }
       }
     },
-    locales: [
-      'pt-BR',
-    ],
+    notifications: {
+      releases: false
+    },
+    tutorials: false,
+    locales: ["pt-BR"],
   },
   bootstrap(app) {
     console.log(app);
