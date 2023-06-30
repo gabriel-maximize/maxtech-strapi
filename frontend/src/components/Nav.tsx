5b4d33d3a6feca75bb7db00f4a3cfe079631a76a
Gabriel Melo <melogoncalvesbiel@gmail.com>
2023-06-30 14:31:20 -0300

    feat: funcionalidade dropdown no subheader

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,7 +1,12 @@
 'use client'
 
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
 import Link from 'next/link';
+
+import { MdKeyboardArrowDown } from "react-icons/md"
 import { useState } from 'react';
+
+import { links } from "@/utils/data/links";
 
 export function Nav() {
     const [show, setShow] = useState(false);
@@ -10,43 +15,51 @@ export function Nav() {
         setShow(show => !show);
     }
 
-    interface HeadersItens {
-        text: string
-        to: string
-        target?: string
-    }
-
-    const headers: HeadersItens[] = [
-        {
-            text: "Início",
-            to: '/',
-        },
-        {
-            text: "Sobre",
-            to: '/sobre',
-        },
-        {
-            text: "FAQ",
-            to: '/faq',
-        },
-        {
-            text: "Notícias",
-            to: '/noticias',
-        },
-    ]
-
     return (
         <nav className={`nav header__nav ${show ? 'nav--open':''}`}>
             <div className="nav__wrap">
                 <ul className="nav__list">
                     {
-                        headers.map((item) => (
-                            <li key={item.text}>
-                                <Link href={item.to} target={item.target || '_self'} className="nav__link">
-                                    {item.text}
-                                </Link>
-                            </li>
-                        ))
+                        links.map((item) => {
+                            return (
+                                <li key={item.text}>
+                                {
+                                    (!item.subitems) ? (
+                                        <Link href={item.to} target={item.target || '_self'} className="nav__link">
+                                            {item.text}
+                                        </Link>
+                                    ) : (
+                                        <DropdownMenu.Root>
+                                            <DropdownMenu.Trigger asChild>
+                                                <Link href={item.to} target={item.target || '_self'} className="nav__link">
+                                                    {item.text}
+                                                    <MdKeyboardArrowDown size={20} />
+                                                </Link>
+                                            </DropdownMenu.Trigger>
+
+                                            <DropdownMenu.Portal>
+                                                <DropdownMenu.Content className="dropdown__content" sideOffset={5} side="bottom" align="start">
+                                                    {
+                                                        item.subitems.map((subitem) => (
+                                                            <DropdownMenu.Item key={subitem.text} className="dropdown__item">
+                                                                <Link
+                                                                    href={subitem.to}
+                                                                    target={subitem.target || '_self'}
+                                                                    className="nav__link nav__link--dropdown"
+                                                                >
+                                                                    {subitem.text}        
+                                                                </Link>
+                                                            </DropdownMenu.Item>
+                                                        ))
+                                                    }
+                                                </DropdownMenu.Content>
+                                            </DropdownMenu.Portal>
+                                        </DropdownMenu.Root>
+                                    )
+                                }
+                                </li>
+                            )
+                        })
                     }
                 </ul>
                 <button className="button nav__contato" onClick={toggleMenu}><span>Fale conosco</span></button>
---
+++ frontend/src/utils/data/links.ts
@@ -0,0 +1,43 @@
+interface HeadersItens {
+  text: string
+  to: string
+  target?: string
+  subitems?: {
+      text: string
+      to: string
+      target?: string
+  }[]
+}
+
+export const links: HeadersItens[] = [
+  {
+      text: "Início",
+      to: '/',
+  },
+  {
+      text: "Serviço",
+      to: '',
+      subitems: [
+          {
+              text: "Limpeza hospitalar",
+              to: '/item-1'
+          },
+          {
+            text: "Limpeza de material cortante",
+            to: "/item-2"
+          }
+      ]
+  },
+  {
+      text: "Sobre",
+      to: '/sobre',
+  },
+  {
+      text: "FAQ",
+      to: '/faq',
+  },
+  {
+      text: "Dicas",
+      to: '/dicas',
+  },
+]
