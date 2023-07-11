0921a72122b572a969e4f6055ec02589ef82d29b
Gabriel Melo <melogoncalvesbiel@gmail.com>
2023-07-11 09:59:34 -0300

    feat: cria componente com titulo

---
+++ frontend/src/components/sections/Servicos.tsx
@@ -0,0 +1,16 @@
+export function Servicos() {
+  return (
+    <section className="servicos">
+      <div className="container">
+        <h2 className="servicos__title">
+          Nossos <strong>serviços</strong>
+        </h2>
+        <h3 className="servicos__subtitle">
+          Para cada necessidade, montamos um plano estratégico de atuação com um
+          único objetivo, solucionar problemas e minimizar possíveis danos à
+          marca dos nossos clientes.
+        </h3>
+      </div>
+    </section>
+  )
+}
